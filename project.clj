16646140a487175441eeb7ee7f6715f89ec22c64
Daniel E. Renfer <duck@kronkltd.net>
2011-09-18 15:40:31 -0400

    update to clojure 1.3

--- project.clj
+++ project.clj
@@ -1,10 +1,13 @@
 (defproject midje "1.3-alpha1"
   :description "A TDD library for Clojure, with an emphasis on mocks"
-  :dependencies [[org.clojure/clojure "[1.2.0,1.2.1]"]
-                 [org.clojure/clojure-contrib "1.2.0"]
-                 [ordered "0.3.0"]
+  :dependencies [[org.clojure/clojure "1.3.0-RC0"]
+                 [org.clojure.contrib/combinatorics "1.3.0-alpha4"]
+                 [org.clojure.contrib/seq "1.3.0-alpha4"]
+                 [org.clojure.contrib/math "1.3.0-alpha4"]
+                 [org.clojure.contrib/ns-utils "1.3.0-alpha4"]
                  [org.clojure/algo.monads "0.1.0"]
+                 [ordered "0.3.0"]
                  [unifycle "0.5.0"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]
-                     [slamhound "1.1.1"]])
-
+  :exclusions [org.clojure/contrib
+               org.clojure/clojure-contrib]
+  :aot [midje.sweet])
